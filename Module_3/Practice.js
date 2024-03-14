ae1ab8b0eb5d20f252bded3ede72352975bbc09e
Rahul-Pala <rahul_pala@hotmail.com>
2024-03-15 08:38:28 +1300

    module 2 content

---
+++ Module_3/Practice.js
@@ -0,0 +1,17 @@
+
+var Name="Rahul"
+var surname="Pala"
+console.log(Name + surname)
+
+surname="Amit"
+console.log(surname)
+
+const numb=100
+console.log(numb)
+
+
+let age;
+let nullAge = null;
+console.log(`${age} == ${nullAge} ? ${age == nullAge}`) // true (uses == to check equivalence)
+console.log(`${age} === ${nullAge} ? ${age === nullAge}`) // false (uses === to check equality)
+
