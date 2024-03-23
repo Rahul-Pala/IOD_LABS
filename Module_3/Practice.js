c2aa0819ff8ab80e34a3d555519f361328f22c6a
Rahul-Pala <rahul_pala@hotmail.com>
2024-03-23 14:27:57 +1300

    Final Module 2 Lab

--- Module_3/Practice.js
+++ Module_3/Practice.js
@@ -15,3 +15,27 @@ let nullAge = null;
 console.log(`${age} == ${nullAge} ? ${age == nullAge}`) // true (uses == to check equivalence)
 console.log(`${age} === ${nullAge} ? ${age === nullAge}`) // false (uses === to check equality)
 
+console.log('hello'); console.log('world');
+
+console.log('hi') 
+console.log('mate')
+
+let nameofstudent = "Rahul"
+console.log(nameofstudent)
+
+nameofstudent=10
+console.log(nameofstudent)
+
+nameofstudent="ridhima"
+console.log(nameofstudent)
+
+console.log("rahul"/60)
+console.log(-90/0)
+
+let one = 1;
+let two = 2;
+let three = 3;
+// standard BODMAS order of operations - use
+//brackets to override
+// below is: 1 + 2 - ( (3 * 2) / 1 ) = 3 - 6
+console.log(one + two - three * two / one); // -3
