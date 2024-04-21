33a367195dfd207d32c52555043b58f2bf466478
Rahul-Pala <rahul_pala@hotmail.com>
2024-04-21 23:56:20 +1200

    miniProjectupdated

---
+++ MiniProject_1/script.js
@@ -0,0 +1,24 @@
+function fetchNews() {
+    fetch('http://localhost:3000/Gelatolist')
+    .then((response) => response.json())
+    .then((json) => {console.log(json);json.forEach(item =>addCard(item.flavor,item.description))}); //this works because the function newscars is not defined by let or const
+}
+fetchNews()
+
+
+
+    function addCard(text, body) {
+    // clone the template
+    const template = 
+    document.getElementById("card-template")
+    .content.cloneNode(true);
+    // populate the template
+    template.querySelector('.card-title').innerText = 
+    text;
+    template.querySelector('.card-text').innerText = 
+    body;
+    // include the populated template into the page
+    document.querySelector('#card-list')
+    .appendChild(template);
+    }
+data.forEach(item =>addCard(item.flavor,item.description));
